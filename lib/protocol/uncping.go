324e9f7ed4116132e820712182c9b8453c50d4c3
Mokhtar <149696460+privatemokhtar@users.noreply.github.com>
2023-12-04 14:44:21 +0330

    Update uncping.go

--- lib/protocol/uncping.go
+++ lib/protocol/uncping.go
@@ -1,7 +1,44 @@
 package protocol
 
+import (
+	"encoding/binary"
+	"io"
+)
+
 type uncPing struct {
 	Time       uint8
 	MAGIC      uint16
 	ClientGUID uint8
+}
+
+func (p *uncPing) Read(loom io.Reader) error {
+	err := binary.Read(loom, binary.BigEndian, &p.Time)
+	if err != nil {
+		return err
+	}
+	err = binary.Read(loom, binary.BigEndian, &p.MAGIC)
+	if err != nil {
+		return err
+	}
+	err = binary.Read(loom, binary.BigEndian, &p.ClientGUID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *uncPing) Write(loom io.Writer) error {
+	err := binary.Write(loom, binary.BigEndian, &p.Time)
+	if err != nil {
+		return err
+	}
+	err = binary.Write(loom, binary.BigEndian, &p.MAGIC)
+	if err != nil {
+		return err
+	}
+	err = binary.Write(loom, binary.BigEndian, &p.ClientGUID)
+	if err != nil {
+		return err
+	}
+	return nil
 }
